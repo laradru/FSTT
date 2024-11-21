410433c5cf49f15a6e33af178d5708216adf0802
laradru <laraherrera0@gmail.com>
2024-11-21 19:50:02 -0300

    Dois grupos

--- analise.R
+++ analise.R
@@ -13,7 +13,9 @@ espessuras$Grupo <- factor(espessuras$Idade, levels = c("7", "8", "9", "10", "11
                            labels = c("7 a 11 anos", "7 a 11 anos", "7 a 11 anos", "7 a 11 anos", "7 a 11 anos", "12 a 18 anos", 
                                       "12 a 18 anos", "12 a 18 anos", "12 a 18 anos", "12 a 18 anos", "12 a 18 anos", "12 a 18 anos"))
 str(espessuras)
-
+table(espessuras$Sexo)
+table(espessuras$Grupo)
+table(espessuras$Sexo, espessuras$Grupo)
 #######################################################################################################################################
 # Espessuras de tecido mole geral:
 #######################################################################################################################################
@@ -312,31 +314,53 @@ by(espessuras$Mid.ramus.D, espessuras$Sexo, stat.desc, basic = FALSE, norm = TRU
 
 
 
-
-
+##############
+# Teste
+##############
 #Paramétrico: Teste-t
 #H0: mu1 = mu2
 #H1: mu1 != mu2
 #alfa: 5%
 #poder: 80%
+t.test(Nasio ~ Sexo, data = espessuras, var.equal = TRUE, na.action = na.exclude, alternative = "two.sided")
+t.test(Mid.supraorbital.E ~ Sexo, data = espessuras, var.equal = TRUE, na.action = na.exclude, alternative = "two.sided")
+t.test(Malar.D ~ Sexo, data = espessuras, var.equal = TRUE, na.action = na.exclude, alternative = "two.sided")
+t.test(Lateral.orbita.E ~ Sexo, data = espessuras, var.equal = FALSE, na.action = na.exclude, alternative = "two.sided")
+t.test(Supraglenoide.E ~ Sexo, data = espessuras, var.equal = TRUE, na.action = na.exclude, alternative = "two.sided")
+#Não paramétrico: Mann-Whitney (Wilcoxon rank-sum test)
+#H0: os dois grupos masc e fem nao diferem quanto a espessura da glabela
+#H1: diferem
+wilcox.test(Rinio ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Filtro.medio ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Prostio ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Infradental ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Supramental ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Pogonio ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mento ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mid.supraorbital.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mid.infraorbital.E ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mid.infraorbital.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Malar.E ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Lateral.orbita.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Zigio.E ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Zigio.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Supraglenoide.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare1s.E ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare1s.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare2s.E ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare2s.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Supraglenoide.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare1i.E ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare1i.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare2i.E ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare2i.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Gonio.E ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Gonio.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Linha.oclusal.E ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Linha.oclusal.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mid.ramus.E ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mid.ramus.D ~ Sexo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
 
-#Não paramétrico: Mann-Whitney
-
-t.test(Glabela ~ Sexo, data = espessuras, paired = FALSE, var.equal = FALSE)
-
-
-
-
-
-
-
-
-
-
-                         
-##############
-# Teste
-##############
 
 ##############
 # Teste-t - Variável Idade
@@ -448,6 +472,185 @@ leveneTest(espessuras$Mid.ramus.D, espessuras$Grupo, center = median)
 espessuras %>% filter(Grupo=="7 a 11 anos") %>% select(Ectomolare2i.D) %>% na.omit() %>% stat.desc(norm = TRUE)
 espessuras %>% filter(Sexo=="Masculino") %>% select(Glabela) %>% na.omit()
 
+##############
+# Teste
+##############
+#Paramétrico: Teste-t
+#H0: mu1 = mu2
+#H1: mu1 != mu2
+#alfa: 5%
+
+t.test(Nasio ~ Grupo, data = espessuras, var.equal = FALSE, na.action = na.exclude, alternative = "two.sided")
+t.test(Filtro.medio ~ Grupo, data = espessuras, var.equal = FALSE, na.action = na.exclude, alternative = "two.sided")
+t.test(Supramental ~ Grupo, data = espessuras, var.equal = FALSE, na.action = na.exclude, alternative = "two.sided")
+t.test(Pogonio ~ Grupo, data = espessuras, var.equal = FALSE, na.action = na.exclude, alternative = "two.sided")
+t.test(Mid.supraorbital.E ~ Grupo, data = espessuras, var.equal = FALSE, na.action = na.exclude, alternative = "two.sided")
+t.test(Malar.E ~ Grupo, data = espessuras, var.equal = FALSE, na.action = na.exclude, alternative = "two.sided")
+t.test(Malar.D ~ Grupo, data = espessuras, var.equal = TRUE, na.action = na.exclude, alternative = "two.sided")
+t.test(Lateral.orbita.D ~ Grupo, data = espessuras, var.equal = FALSE, na.action = na.exclude, alternative = "two.sided")
+t.test(Supraglenoide.E ~ Grupo, data = espessuras, var.equal = TRUE, na.action = na.exclude, alternative = "two.sided")
+t.test(Supraglenoide.D ~ Grupo, data = espessuras, var.equal = FALSE, na.action = na.exclude, alternative = "two.sided")
+t.test(Ectomolare1i.D ~ Grupo, data = espessuras, var.equal = FALSE, na.action = na.exclude, alternative = "two.sided")
+#Não paramétrico: Mann-Whitney (Wilcoxon rank-sum test)
+#H0: os dois grupos masc e fem nao diferem quanto a espessura da glabela
+#H1: diferem
+wilcox.test(Glabela ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Rinio ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Filtro.medio ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Prostio ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Infradental ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mento ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mid.supraorbital.D ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mid.infraorbital.E ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mid.infraorbital.D ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Lateral.orbita.E ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Zigio.E ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Zigio.D ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Supraglenoide.D ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare1s.E ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare1s.D ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+
+wilcox.test(Ectomolare2s.E ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare2s.D ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+
+wilcox.test(Ectomolare1i.E ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+
+wilcox.test(Ectomolare2i.E ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Ectomolare2i.D ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+
+wilcox.test(Gonio.E ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Gonio.D ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Linha.oclusal.E ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Linha.oclusal.D ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mid.ramus.E ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
+wilcox.test(Mid.ramus.D ~ Grupo, data = espessuras, na.action = na.exclude, alternative = "two.sided")
 
 
+
+
+### tabelas
+library(writexl)
+tabelasexo <- resultado.sexo[ , c(1:4, 6, 7)]
+tabelaidade <- resultado.idade.grupo[ , c(1:4, 6, 7)]
+
+tabelasexo <- tabelasexo %>% arrange(variavel)
+write_xlsx(tabelasexo, "tabelasexo.xlsx")
+
+mediaf <- tabelasexo %>% filter(Sexo == "Feminino") %>% select(variavel, Média)
+mediam <- tabelasexo %>% filter(Sexo == "Masculino") %>% select(variavel, Média)
+df<- mediaf$Média - mediam$Média
+var <- mediaf$variavel
+dft<- data.frame(var, df)
+dft
+
+write_xlsx(dft, "dif.xlsx")
+
+tabelaidade <- tabelaidade %>% arrange(variavel)
+write_xlsx(tabelaidade, "tabelaidade.xlsx")
+
+media1 <- tabelaidade %>% filter(Grupo == "7 a 11 anos") %>% select(variavel, Média)
+media2 <- tabelaidade %>% filter(Grupo == "12 a 18 anos") %>% select(variavel, Média)
+df<- media1$Média - media2$Média
+var <- media1$variavel
+dft<- data.frame(var, df)
+dft
+write_xlsx(dft, "dif.xlsx")
+
+
+
+
+
+
+
+
+### Comparação com Kuhnen
+library(dplyr)
+separado <- as.data.frame(kunen)
+separado <- separado[,1:6]
+
+kunen.sexo <- as.data.frame(kunen)
+kunen.sexo <- kunen.sexo %>% na.exclude()
+kunen.sexo <- kunen.sexo %>% rename(variavel = Variável)
+str(kunen.sexo)
+kunen.sexo$autor <- rep("Kuhnen", 38)
+
+
+kondo.sexo <- data.frame(resultado.sexo$Sexo, resultado.sexo$variavel, resultado.sexo$Média, resultado.sexo$Desvio.padrão, resultado.sexo$n)
+kondo.sexo <- kondo.sexo %>% rename(n = resultado.sexo.n, variavel = resultado.sexo.variavel)
+str(kondo.sexo)
+kondo.sexo$autor <- rep("Kondo", 70)
+
+
+kondo.sexo <- kondo.sexo[kondo.sexo$variavel %in% kunen.sexo$variavel, ]
+comparacao <- rbind(kondo.sexo, kunen.sexo)
+
+
+
+library(ggplot2)
+comparacao %>% ggplot(aes(variavel, Media, fill= autor, group = autor)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.7) + 
+  geom_errorbar(aes(ymin = Media - sd, ymax = Media + sd), position = position_dodge(width = 0.8), width = 0.25) +
+  labs(x = "Ponto craniométrico", y = "Espessura", fill= "Autor", title = "Espessura média (em mm) e desvio-padrão para cada ponto craniométrico, por sexo e autor") +
+  facet_wrap(~Sexo) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(size=0.2, colour="black"))
+
+
+
+
+
+
+
+
+
+
+
+kunen.idade <- as.data.frame(kunen)
+kunen.idade <- kunen.idade %>% na.exclude()
+kunen.idade <- kunen.idade %>% rename(variavel = Variável)
+str(kunen.idade)
+kunen.idade$autor <- rep("Kuhnen", 38)
+
+
+kondo.idade <- data.frame(resultado.idade.grupo$Grupo, resultado.idade.grupo$variavel, resultado.idade.grupo$Média, resultado.idade.grupo$Desvio.padrão, resultado.idade.grupo$n)
+kondo.idade <- kondo.idade %>% rename(n = resultado.idade.grupo.n, variavel = resultado.idade.grupo.variavel, Idade = resultado.idade.grupo.Grupo, Media = resultado.idade.grupo.Média, sd = resultado.idade.grupo.Desvio.padrão)
+str(kondo.idade)
+kondo.idade$autor <- rep("Kondo", 70)
+kondo.idade <- kondo.idade %>% na.exclude()
+
+
+
+kondo.idade <- kondo.idade[kondo.idade$variavel %in% kunen.idade$variavel, ]
+comparacao.idade <- rbind(kondo.idade, kunen.idade)
+
+
+library(ggplot2)
+comparacao.idade %>% ggplot(aes(variavel, Media, fill= autor, group = autor)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.7) + 
+  geom_errorbar(aes(ymin = Media - sd, ymax = Media + sd), position = position_dodge(width = 0.8), width = 0.25) +
+  labs(x = "Ponto craniométrico", y = "Espessura",fill= "Autor", title = "Espessura média (em mm) e desvio-padrão para cada ponto craniométrico, por idade e autor") +
+  facet_wrap(~Idade) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(size=0.2, colour="black"))
+
+
+
+
+
+
+teste<- kunen.sexo %>% arrange(Sexo)
+diferenca.sexo <- data.frame(kondo.sexo$Media - teste$Media)
+dif.sexo.autores <- data.frame(kondo.sexo$Sexo,kondo.sexo$variavel, kondo.sexo$Media, teste$Media, diferenca.sexo)
+dif.sexo.autores <- dif.sexo.autores %>% rename(Sexo = kondo.sexo.Sexo, Variavel = kondo.sexo.variavel, Media.Kondo = kondo.sexo.Media, Media.Kuhnen = teste.Media, Diferenca = kondo.sexo.Media...teste.Media)
+dif.sexo.autores <-  dif.sexo.autores %>% arrange(Variavel)
+dif.sexo.autores <- dif.sexo.autores %>% select(Variavel, Sexo, Media.Kondo, Media.Kuhnen, Diferenca)
+
+
+kunen.idade$Idade <- as.factor(kunen.idade$Idade)
+kunen.idade$Idade <- factor(kunen.idade$Idade, levels = c("7 a 11 anos", "12 a 18 anos"))
+str(kunen.idade)
+teste <- kunen.idade %>% arrange(Idade)
+teste <- teste %>% filter(variavel != c("Ectomolare2i.E", "Ectomolare2s.E"))
+diferenca.idade <- data.frame(kondo.idade$Media - teste$Media)
+dif.idade.autores <- data.frame(kondo.idade$variavel, kondo.idade$Idade, kondo.idade$Media, teste$Media, diferenca.idade)
+dif.idade.autores <- dif.idade.autores %>% rename(Idade = kondo.idade.Idade, Variavel = kondo.idade.variavel, Media.Kondo = kondo.idade.Media, Media.Kuhnen = teste.Media, Diferenca = kondo.idade.Media...teste.Media)
+dif.idade.autores <-  dif.idade.autores %>% arrange(Variavel)
 
